5b38bc36ee07c17f939901ed2a39349519d3468a
Rishabh Mishra <rishabhm1508@gmail.com>
2020-11-05 01:35:09 +0530

    spaceX theme

--- src/app/components/launch-filter/launch-filter.component.ts
+++ src/app/components/launch-filter/launch-filter.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { YEARS } from "src/app/shared/space-x.constant";
 
 @Component({
-  selector: 'app-launch-filter',
-  templateUrl: './launch-filter.component.html',
-  styleUrls: ['./launch-filter.component.scss']
+  selector: "app-launch-filter",
+  templateUrl: "./launch-filter.component.html",
+  styleUrls: ["./launch-filter.component.scss"],
 })
 export class LaunchFilterComponent implements OnInit {
+  years = YEARS;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-    
-  }
-
+  ngOnInit() {}
 }
